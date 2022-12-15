a99e2babbd952e3b300f4ab2700c22d0b209ef8d
Tobias Fritz <97955191+tobias-fritz@users.noreply.github.com>
2022-12-16 00:16:48 +0100

    Added Fibonacci series

--- brain_teasers.py
+++ brain_teasers.py
@@ -4,14 +4,18 @@
 # Author: Tobias Fritz
 #===============================================================================================
 
+#===============================================================================================
+# For a cash value, return the minimum amount of euro coins to reach it
+#===============================================================================================
 
-# For a sum of results, return the minimum amount of euro coins 
 def automat(cash, wallet = []):
   ''' Take an amount of cash in cent (int) and return the minimal amount of coins as a list
   param:  
           cash:   int:  the amount to be payed in cent
           wallet: list: coins that are used
   '''
+    
+    assert cash.type == int
     
     #coin dict
     coins = [200,100,50,20,10,5,2,1]
@@ -27,4 +31,24 @@ def automat(cash, wallet = []):
         elif (cash-coin) > 0 :
             wallet.append(coin)
             return (automat(cash-coin,wallet))
+
+#===============================================================================================        
+# implement the fibonaci series recursively
+#===============================================================================================
+
+def fibonacci_series(position):
+    ''' compute the nth position of the fibonacci series
+    param:
+            position: int: nth position of the fibonacci series
+    '''
+    
+    assert position == int
+    
+    if position == 0:
+        return 0
+    elif position == 1:
+        return 1
+    else:
+        return (fibonacci_series(position-1) + fibonacci_series(position-2))
+  
           
